d56e5b707ef21eddb7bd55d4c46593dd8e5c032e
Antanas Tamašauskas <55793899+erelisvanagelis@users.noreply.github.com>
2022-01-14 22:53:12 +0200

    Add project files.

---
+++ OpenTTD Launcher/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using OpenTTD_Launcher.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenTTD_Launcher
+{
+
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ OpenTTD Launcher/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTTD_Launcher.ViewModels
+{
+    class MainViewModel : Screen
+    {
+        private bool _autoLauch;
+        private bool _autoClose;
+        private string _exePath;
+        private string _configPath;
+
+        public bool AutoLauch { get => _autoLauch; set => _autoLauch = value; }
+        public bool AutoClose { get => _autoClose; set => _autoClose = value; }
+        public string ExePath { get => _exePath; set => _exePath = value; }
+        public string ConfigPath { get => _configPath; set => _configPath = value; }
+
+        public MainViewModel()
+        {
+
+
+        }
+
+
+    }
+}
